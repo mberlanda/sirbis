b4b00b874a3d1be8a4074d02a095fd08d6679fc8
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-01-16 09:00:27 +0100

    added quiz show

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,4 +2,8 @@ class QuizzesController < ApplicationController
   def index
     @quizzes = Quiz.includes(:questions).all
   end
+
+  def show
+    @quiz = Quiz.includes(questions: :answers).find(params[:id])
+  end
 end
